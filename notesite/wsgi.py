7f58783b060ae2fbe9ef1b66bc52799f3f15380c
ataman <atamanenkoalex90@gmail.com>
2023-05-26 03:02:20 +0300

    Change root directory

--- notesite/wsgi.py
+++ notesite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for notesite project.
+WSGI config for notesite_1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notesite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notesite_1.settings")
 
 application = get_wsgi_application()
